ac2d518563f0adcc1c7af0d79c6471c750e5d0ef
Threkork <threkork@hotmail.com>
2024-01-12 20:44:20 +0800

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Lrc-Paper/",
+  build: {
+    outDir: "docs"
+  },
   plugins: [
     vue(),
   ],
